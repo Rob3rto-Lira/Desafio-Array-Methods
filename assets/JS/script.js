cba2d8dd3c41e66049e8b1fff34f479c09df5901
Rob3rto-Lira <rliraleon@gmail.com>
2025-02-02 22:26:18 -0300

    Finished

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,6 @@
 const input = document.getElementById('input');
 const agregar = document.getElementById('agregar');
-const total = document.getElementById('total');
+let total = document.getElementById('total');
 const realizada = document.getElementById('realizada');
 const id = document.getElementById('ID');
 const tareasContainer = document.getElementById('tareas-container');
@@ -8,7 +8,14 @@ const tarea = document.getElementById('tarea');
 let checkbox = document.getElementsByClassName('checkbox');
 let eliminar = document.getElementsByClassName('eliminar');
 
-const tareas = [ { ID: Date.now(), Tarea: 'Hacer mercado', Completada: false } ];
+const tareas = [ 
+    { ID: Date.now(), Tarea: 'Hacer mercado', Completada: false },
+    { ID: Date.now(), Tarea: 'Estudiar para la prueba', Completada: false },
+    { ID: Date.now(), Tarea: 'Pasear a Bobby', Completada: false }
+ ];
+
+let Total = Number(total.textContent)
+let Realizada = Number(realizada.textContent)
 
 
 function render() {
@@ -38,6 +45,9 @@ function render() {
     // Reinicia el string
     resetInput= () => input.value = '';
     resetInput();
+    // Actualiza el total de tareas
+    Total = tareas.length;
+    total.innerHTML = Total;
 }
 
 document.addEventListener('DOMContentLoaded', render());
@@ -66,6 +76,9 @@ tareasContainer.addEventListener('click', (e) => {
         const tareaId = parseInt(tareaDiv.dataset.id);
         const index = tareas.findIndex(task => task.ID === tareaId);
         tareas[index].Completada = target.checked;
+        // Actualiza el total de tareas realizadas
+        Realizada = tareas.filter(task => task.Completada).length;
+        realizada.innerHTML = Realizada;
     }
 })
 
